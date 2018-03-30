424acd8dd0264dce79ac8445f51499755d348273
Sahana Balram <sahana2008balram@gmail.com>
2018-03-30 15:57:32 -0700

    Install dependencies

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+// Used for easy parsing user-agent for response
+const useragent = require("express-useragent");
 
 const app = module.exports = express();
 const PORT = process.env.port || 3000;
@@ -10,14 +12,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 app.use(cors());
+app.use(useragent.express());
 
 // API url
 var apiURL = "/api/whoami";
 app.get(apiURL,function(request,response){
-    var language = request.acceptsLanguages;
-    var software;
+    var language = request.acceptsLanguages();
+    var software = "OS: " + request.useragent.os +   " Browser: " + request.useragent.browser;
     var ipAddress = request.ip;
-    response.json({"ipAddress": ipAddress});
+    response.json({"ipAddress": ipAddress, "language": language[0],"software": software});
 });
 app.listen(PORT,function(){
     console.log("App is listening on:" + PORT);
